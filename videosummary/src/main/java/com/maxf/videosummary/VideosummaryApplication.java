4b60af7d67bddd9c94899bdb7b6a498cd3de8d35
ma.xiaofeng <1231231231231>
2018-12-19 13:34:33 +0800

    目录结构大改

---
+++ videosummary/src/main/java/com/maxf/videosummary/VideosummaryApplication.java
@@ -0,0 +1,14 @@
+package com.maxf.videosummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideosummaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideosummaryApplication.class, args);
+    }
+
+}
+
---
+++ videosummary/src/test/java/com/maxf/videosummary/VideosummaryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.maxf.videosummary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideosummaryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
