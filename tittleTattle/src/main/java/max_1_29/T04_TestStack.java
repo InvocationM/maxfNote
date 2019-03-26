52fc2f7cbe3f94c3189f7d17447b9bb92ec9e276
maxiaofeng <601750328@qq.com>
2019-03-27 00:17:02 +0800

    注解的原理

---
+++ tittleTattle/src/main/java/max_1_29/T04_TestStack.java
@@ -0,0 +1,27 @@
+package max_1_29;
+
+/**
+ * 描述:
+ * ----测试栈信息的打印顺序
+ *
+ * @author Administrator
+ * @create 2019-03-26 20:45
+ */
+public class T04_TestStack {
+
+    public static void main(String[] args) {
+
+    int i = 0;
+
+    method1(i);
+
+    }
+
+    public static void method1(int i) {
+
+        System.out.println(1/i);
+    }
+
+
+
+}
