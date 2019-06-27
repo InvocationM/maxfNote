a063ac157979608e83c76d7871769300118c8803
ma.xiaofeng <1231231231231>
2019-06-27 15:19:53 +0800

    各种小测试

---
+++ tittleTattle/src/main/java/max_06_20/TestWriter.java
@@ -0,0 +1,22 @@
+package max_06_20;
+
+/**
+ * 描述:
+ * ----
+ *
+ * @author ma.xiaofeng
+ * @create 2019-06-26 15:23
+ */
+public class TestWriter {
+
+    public static void main(String[] args) {
+
+        int times = Integer.MAX_VALUE;
+
+//        while () {
+            System.out.println(times --> 0);
+
+//        }
+    }
+
+}
