6a36532f4be08bf493084ba246944cc66c832376
maxiaofeng <601750328@qq.com>
2019-06-27 15:16:11 +0800

    并发集合 整理

---
+++ tittleTattle/src/main/java/max_06_26/T03_2Colon.java
@@ -0,0 +1,20 @@
+package max_06_26;
+
+/**
+ * 描述:
+ * ----
+ *
+ * @author Administrator
+ * @create 2019-06-27 14:51
+ */
+public class T03_2Colon {
+
+    public static void main(String[] args) {
+
+//        System.out.println(String::charAt1);
+
+//        System.out.println(String::valueOf(1));
+
+    }
+
+}
