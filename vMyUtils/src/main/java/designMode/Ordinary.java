3cb43a4ec42b0b3dc9c9f8357f7d3c1233678c81
maxiaofeng <601750328@qq.com>
2019-07-23 23:58:42 +0800

    mybatis第一天 数据源解读

---
+++ vMyUtils/src/main/java/designMode/Ordinary.java
@@ -0,0 +1,16 @@
+package designMode;
+
+/**
+ * 描述:
+ * ----
+ *
+ * @author Administrator
+ * @create 2019-07-18 23:03
+ */
+public class Ordinary {
+
+    public void  method() {
+        System.out.println(" method");
+    }
+
+}
