bf53a4781284bb25c0a2e1fb2b9808a8238e833d
maxiaofeng <601750328@qq.com>
2019-07-28 11:07:27 +0800

    mybatis第二天

---
+++ tittleTattle/src/main/java/max_07_11/T04_getName.java
@@ -0,0 +1,22 @@
+package max_07_11;
+
+/**
+ * 描述:
+ * ----
+ *
+ * @author Administrator
+ * @create 2019-07-24 22:47
+ *
+ *          class的getName就是得到类的全名称
+ */
+public class T04_getName {
+
+    public static void main(String[] args) {
+
+        String string = "";
+
+        System.out.println(string.getClass().getName());
+
+    }
+
+}
