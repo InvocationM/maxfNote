2a81b08dbc1994e6ed5baccaf2df8be621c2bfa4
maxiaofeng <601750328@qq.com>
2019-11-05 14:28:26 +0800

    add

--- tittleTattle/src/main/java/max_10_11/VoidClass.java
+++ tittleTattle/src/main/java/max_10_11/VoidClass.java
@@ -12,10 +12,36 @@ public class VoidClass {
     public static void main(String[] args) {
 
         Class<Void> voidClass = void.class;
+//
+//        System.out.println(voidClass.getName());
+//        System.out.println(voidClass.getClassLoader());
+//        System.out.println(voidClass.getSigners());
+//        Integer num1 = 400;
+//         int num2 = 400;
+//        System.out.println(num1 == num2);
+//
+//        int a = 200;
+//        if (a instanceof Object) {
+//
+//        }
+//
+//        boolean assignableFrom = Object.class.isAssignableFrom(int.class);
+//        System.out.println(assignableFrom);
+//        int a = 10;
+//        long b = 10L;
+//
+//        System.out.println(a == b);
+        int zhuan=Integer.parseInt("111");
+        int zhuanyi=Integer.valueOf("11111");
 
-        System.out.println(voidClass.getName());
-        System.out.println(voidClass.getClassLoader());
-        System.out.println(voidClass.getSigners());
+
+    }
+
+    public static Long convert(Object obj) {
+        if (obj instanceof Integer) {
+            System.out.println("sssssssssssssssssssssssssssss");
+        }
+        return -1L;
     }
 
 }
