012129d04fe52fc4c0ed3c2fbcff7ee93639ca7f
maxiaofeng <601750328@qq.com>
2019-11-13 09:43:49 +0800

    优化

---
+++ tittleTattle/src/main/java/max_11_8/TestIOx.java
@@ -0,0 +1,21 @@
+package max_11_8;
+
+/**
+ * 描述:
+ * ----  十六进制数字，对应的 asc码字符
+ *
+ * @author Administrator
+ * @create 2019-11-08 16:06
+ */
+public class TestIOx {
+
+    public static void main(String[] args) {
+
+//        System.out.println(0x20);   十进制 32
+
+        // char 字符-数字对应      0x20对应的是空格。  小于32的，都是些 虚操作符
+        char i = 0x21;
+        System.out.println(i);
+    }
+
+}
