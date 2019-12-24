ec9a418f250cfe2d1fd50f0deea61748b520d6e3
maxiaofeng <601750328@qq.com>
2019-12-24 11:07:41 +0800

    add

---
+++ tittleTattle/src/main/java/max_12_6/TestDouble.java
@@ -0,0 +1,26 @@
+package max_12_6;
+
+/**
+ * 描述:
+ * ----测试Doublel.valueOf 空字符串
+ *
+ *                  直接抛出异常
+ *
+ * @author Administrator
+ * @create 2019-12-18 17:06
+ */
+public class TestDouble {
+
+    public static void main(String[] args) {
+
+//        Double aDouble = Double.valueOf("");
+
+//        Integer integer = Integer.valueOf("");
+
+//        System.out.println(integer);
+
+        System.out.println(-Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+}
