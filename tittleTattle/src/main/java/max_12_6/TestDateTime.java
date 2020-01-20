a1dedc74ef8f9dd2eb3d62f8bb91efe1b32eaefc
maxiaofeng <601750328@qq.com>
2020-01-20 16:07:15 +0800

    add

---
+++ tittleTattle/src/main/java/max_12_6/TestDateTime.java
@@ -0,0 +1,17 @@
+package max_12_6;
+
+/**
+ * 描述:
+ * ----测试new date 和new datetime的区别
+ *
+ * @author Administrator
+ * @create 2020-01-19 13:41
+ */
+public class TestDateTime {
+
+    public static void main(String[] args) {
+
+//        ClassLayout
+    }
+
+}
---
+++ tittleTattle/src/main/java/max_12_6/Testor.java
@@ -0,0 +1,25 @@
+package max_12_6;
+
+/**
+ * 描述:
+ * ----
+ *
+ * @author Administrator
+ * @create 2020-01-07 18:31
+ */
+public class Testor {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 100; i++) {
+
+            System.out.println("---------------------" + i);
+
+            Thread.sleep(10000);
+        }
+
+        String s= "";
+        s.length();
+
+    }
+}
